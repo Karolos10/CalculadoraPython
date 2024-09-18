c8b4ff220f784c01c0fa2b5235d749b3adc1df7e
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2024-09-17 21:51:29 -0500

    Implementation of logic of numerical buttons

--- calculadora.py
+++ calculadora.py
@@ -5,52 +5,62 @@ miFrame = Frame(raiz)
 miFrame.pack()
 
 #Pantalla de la calculadora
-pantalla = Entry(miFrame)
+
+numeroPantalla = StringVar()
+
+pantalla = Entry(miFrame, textvariable=numeroPantalla)
 pantalla.grid(row=1, column=1, padx=10, pady=10, columnspan=4)
 pantalla.config(bg="black", fg="#03f943", justify="right")
 
+#-----------------------------------FUNCIONES-----------------------------------
+
+def numeroPulsado(num):
+    numeroPantalla.set(numeroPantalla.get() + num)
+
 #-----------------------------------FILA 1-----------------------------------
-boton7 = Button(miFrame, text="7", width=3)
+boton7 = Button(miFrame, text="7", width=3, command=lambda:numeroPulsado("7"))
 boton7.grid(row=2, column=1)
-boton8 = Button(miFrame, text="8", width=3)
+boton8 = Button(miFrame, text="8", width=3, command=lambda:numeroPulsado("8"))
 boton8.grid(row=2, column=2)
-boton9 = Button(miFrame, text="9", width=3)
+boton9 = Button(miFrame, text="9", width=3, command=lambda:numeroPulsado("9"))
 boton9.grid(row=2, column=3)
 bontonDiv= Button(miFrame, text="/", width=3)
 bontonDiv.grid(row=2, column=4)
 
 #-----------------------------------FILA 2-----------------------------------
 
-boton4 = Button(miFrame, text="4", width=3)
+boton4 = Button(miFrame, text="4", width=3, command=lambda:numeroPulsado("4"))
 boton4.grid(row=3, column=1)
-boton5 = Button(miFrame, text="5", width=3)
+boton5 = Button(miFrame, text="5", width=3, command=lambda:numeroPulsado("5"))
 boton5.grid(row=3, column=2)
-boton6 = Button(miFrame, text="6", width=3)
+boton6 = Button(miFrame, text="6", width=3, command=lambda:numeroPulsado("6"))
 boton6.grid(row=3, column=3)
 bontonMult= Button(miFrame, text="*", width=3)
 bontonMult.grid(row=3, column=4)
 
 #-----------------------------------FILA 3-----------------------------------
 
-boton1 = Button(miFrame, text="1", width=3)
+boton1 = Button(miFrame, text="1", width=3, command=lambda:numeroPulsado("1"))
 boton1.grid(row=4, column=1)
-boton2 = Button(miFrame, text="2", width=3)
+boton2 = Button(miFrame, text="2", width=3, command=lambda:numeroPulsado("2"))
 boton2.grid(row=4, column=2)
-boton3 = Button(miFrame, text="3", width=3)
+boton3 = Button(miFrame, text="3", width=3, command=lambda:numeroPulsado("3"))
 boton3.grid(row=4, column=3)
 bontonRest= Button(miFrame, text="-", width=3)
 bontonRest.grid(row=4, column=4)
 
 #-----------------------------------FILA 4-----------------------------------
 
-boton0 = Button(miFrame, text="0", width=3)
+boton0 = Button(miFrame, text="0", width=3, command=lambda:numeroPulsado("0"))
 boton0.grid(row=5, column=1)
-botonComa = Button(miFrame, text=",", width=3)
+botonComa = Button(miFrame, text=",", width=3, command=lambda:numeroPulsado("."))
 botonComa.grid(row=5, column=2)
 botonIgual = Button(miFrame, text="=", width=3)
 botonIgual.grid(row=5, column=3)
 bontonSum= Button(miFrame, text="+", width=3)
 bontonSum.grid(row=5, column=4)
+
+
 
 
 
